53dffe109ab4f9103ac1e22ec2e0ec4e0d7a6be3
Pranav Chaturvedi <pranav.g10@gmail.com>
2019-04-01 17:09:12 +0530

    Version Bump: 0.2

--- init.rb
+++ init.rb
@@ -6,6 +6,6 @@ Redmine::Plugin.register :google_oauth2 do
   name 'Google Oauth2'
   author 'Pranav Chaturvedi'
   description 'Google Oauth2 plugin for Redmine'
-  version '0.1'
+  version '0.2'
   url 'https://github.com/pranav-g10/redmine-google-oauth2.git'
 end

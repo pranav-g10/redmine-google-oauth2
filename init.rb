f92d83bbf7a00e83f9b2cc0aa3806f686a689687
Pranav Chaturvedi <pranav.g10@gmail.com>
2019-04-01 15:43:55 +0530

    Plugin details and dependencies

---
+++ init.rb
@@ -0,0 +1,9 @@
+require 'redmine'
+
+Redmine::Plugin.register :google_oauth2 do
+  name 'Google Oauth2'
+  author 'Pranav Chaturvedi'
+  description 'Google Oauth2 plugin for Redmine'
+  version '0.1'
+  url 'https://github.com/pranav-g10/redmine-google-oauth2.git'
+end
